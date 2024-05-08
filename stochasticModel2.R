c5c2077f8af5b34f87a8af80226bfde600d8fef5
juansvs <juans.vs@gmail.com>
2024-05-08 13:10:27 +0200

    updates

--- stochasticModel2.R
+++ stochasticModel2.R
@@ -1,150 +1,139 @@
 library(tidyverse)
-
+library(doParallel)
 ####### functions #####
-# Rates for 6 possible events: transmission to sp.1, transmission to sp.2
-# recovery of sp.1, recovery of sp.2, shedding, mortality
+# Rates for 4 possible events: transmission, recovery, appearance of feces in the environment, and disappearance of feces
+
 nxtvnt <- function(X) {
   # give names to the vector entries
-  S1 <- X[1]; S2 <- X[2]
-  I1 <- X[3]; I2 <- X[4]
-  E <- X[5]
+  S <- X[1] 
+  I <- X[2]
+  E <- X[3]
   # calculate the mean rate at which each event is happening
-  trans1 <- b1*S1*E
-  trans2 <- b2*S2*E
-  recov1 <- r1*I1
-  recov2 <- r2*I2
-  shed <- l1*I1+l2*I2
-  mrtl <- E*g
+  trans <- beta*S*E
+  recov <- rho*I
+  shed <- lambda0
+  mrtl <- E*mu
   # combine the rates into a single vector
-  rates <- c(trans1, trans2, recov1, recov2, shed, mrtl)
+  rates <- c(trans, recov, shed, mrtl)
   # calculate the times to the next events;
-  # draw 6 random numbers from a uniform distribution, determine the log
+  # draw 4 random numbers from a uniform distribution, determine the log
   # and multiply by the inverse of the corresponding rate
-  deltas <- -1/rates*log(runif(6))
+  deltas <- -1/rates*log(runif(4))
   return (list(event=which.min(deltas), time=min(deltas)))
 }
 
+updtVec <- function(event) {
+  switch (event$event,
+          c(-1, 1, 0), # transmission
+          c(1, -1, 0), # recovery, adult parasite mortality
+          c(0, 0, 1), # scat deposition
+          c(0, 0, -1)# scat disappearance/loss of infectiousness
+          )
+}
+  
 
-# Create object to store results
-res <- data.frame(beta2 = betas[1], 
-                  iter=1, 
-                  t = 0, 
-                  S1 = n0[1], 
-                  S2 = n0[2], 
-                  I1 = n0[3], 
-                  I2 = n0[4], 
-                  E = n0[5])
+#### Load parameters ####
+source('params2.R')
+beta_orig <- beta
+mu_orig <- mu
+mb_scenarios <- expand.grid(mu=mu_orig*deltas, beta=beta_orig*deltas)[-1,]
 
-fadeoutT <- data.frame(beta2 = 0, t = 0)
+#### setup parallel computation ####
+cl <- makeCluster(6)
+registerDoParallel(cl)
 
-##### changing values of beta ####
-microbenchmark::microbenchmark(
-  for (b2 in betas) {
-    for (i in 1:nsims) {
-      if (i>1) {
-        res <- rbind(res, c(i, 0, n0))
-      }
-      
-      # Set initial objects that will be updated as the simulation progresses
-      N <- n0
-      t <- 0
-      
-      counter <- 0
-      day <- 1
-      while (t<730) {
-        event <- nxtvnt(N)
-        
-        updtVec <- switch (event$event,
-                           c(-1, 0, 1, 0, 0),
-                           c(0, -1, 0, 1, 0),
-                           c(1, 0,-1, 0, 0),
-                           c(0, 1, 0, -1, 0),
-                           c(0, 0, 0, 0, 1),
-                           c(0, 0, 0, 0, -1)
-        )
-        
-        N <- N+updtVec
-        if (N[5]==0) {fadeoutT <- rbind(fadeoutT, c(b2, t)); break}
-        t <- t+event$time
-        # if (event$event <=4) {
-        #   res <- rbind(res, c(i, t, N))
-        # }
-        counter <- counter + 1
-        if (ceiling(t)>day) {
-          day <- ceiling(t)
-          res <- rbind(res, c(b2, i, t, N))
-          
-        }
-        # day <- ceiling(t)
-        # if (counter %% 50 ==0) {
-        #   cat("Iteration:",i, "Step:", counter, "time =", t, "Pop sizes =",N, "\n")
-        # }
-      }
-    }
-    
-  }
-  )
+#### Simulation output ####
+# Open output file to store results. We can use a summarized output that only retains the first time to invasion
+outfile <- paste(Sys.Date(),'_','simOutput.csv',sep='')
+# outInvT <- 'stochastic_output_time.csv'
+outfileconn <- file(outfile, open = 'w'); cat('beta','mu','iter', 'prev','tinv', file = outfileconn, sep=',', fill = T)
+# outInvTconn <- file(outInvT, open = 'w'); cat('beta','iter','t', file = outInvTconn, sep = ',', fill = T)
 
-##### different initial populations sizes ####
-resN0 <- data.frame(n2_0 = N2s[1], iter=1, t = 0, S1 = n0[1], S2 = N2s[1], I1 = n0[3], I2 = n0[4], E = n0[5])
-fadeoutN0 <- data.frame(n0 = N2s[1], t = 0)
-b2 <- betas[2]
-# parameter loop. Change some parameter and do several iterations with that parameter value
-
-for (no in N2s) {
-  n0 <- c(S1 = 29, S2 = no, I1 = 1, I2 = 0, E = 0)
-  # replicates loop, do the same thing for a number of stochastic simulations
+#### run simulation ####
+# bencmarking
+# ptime <- system.time({
+  # to run in parallel use this line:
+  # res <- foreach (d = seq_len(length.out = dim(mb_scenarios)[1]), .combine = rbind) %dopar% {
+for (d in seq_len(length.out = dim(mb_scenarios)[1])) {
+  beta <- mb_scenarios[d,'beta']
+  mu <- mb_scenarios[d,'mu']
+  # foreach (i = 1:nsims, .combine = cbind) %dopar% {
   for (i in 1:nsims) {
-    if (i>1) {
-      resN0 <- rbind(resN0, c(i, 0, n0))
-    }
-    
     # Set initial objects that will be updated as the simulation progresses
     N <- n0
     t <- 0
-    
+    # cat(beta,i, t, N, file = outfileconn, sep=',', fill = T) # write initial state to file
     counter <- 0
     day <- 1
-    # time loop, while time is below two years, keep calculating the next event and updating the population vector accordingly
-    while (t<730) {
+    invaded <- F 
+    tinv <- integer()
+    while (t<sims.duration) {
       event <- nxtvnt(N)
-      
-      updtVec <- switch (event$event,
-                         c(-1, 0, 1, 0, 0),
-                         c(0, -1, 0, 1, 0),
-                         c(1, 0,-1, 0, 0),
-                         c(0, 1, 0, -1, 0),
-                         c(0, 0, 0, 0, 1),
-                         c(0, 0, 0, 0, -1)
-      )
-      
-      N <- N+updtVec
-      if (N[5]==0) {fadeoutN0 <- rbind(fadeoutN0, c(no, t)); break}
+      N <- N+updtVec(event)
       t <- t+event$time
-      # if (event$event <=4) {
-      #   res <- rbind(res, c(i, t, N))
-      # }
-      counter <- counter + 1
-      if (ceiling(t)>day) {
-        day <- ceiling(t)
-        resN0 <- rbind(resN0, c(no, i, t, N))
-        
+      if (event$event==1 && !invaded) {
+        tinv <- t
+        invaded <- T
       }
-      # day <- ceiling(t)
-      # if (counter %% 50 ==0) {
-      #   cat("Iteration:",i, "Step:", counter, "time =", t, "Pop sizes =",N, "\n")
+
+      # counter <- counter + 1
+      # if (ceiling(t)>day) {
+      #   day <- ceiling(t)
+      #   cat(beta,i, round(t,2),N,file = outfileconn, sep=',', fill = T)
       # }
     }
+    prev <- N[2]/(N[1]+N[2])
+    cat(beta, mu, i, round(prev, 2), round(tinv, 2), file=outfileconn, sep=',', fill = T)
+    # c(beta, mu, i, round(prev, 2), round(tinv, 2))#for parallel version
   }
-  
-}
+};close(outfileconn)
 
-######### analysis #######
-res %>% group_by(iter) %>% filter(t>700) %>% transmute(prev1 = I1/(S1+I1), prev2 = I2/(S2+I2)) %>% ungroup() %>% 
-  summarize(p1 = mean(prev1), sd1 = sd(prev1), p2 = mean(prev2), sd2 = sd(prev2))
-# quantify the proportion of disease fading out.
-res %>% filter(t == max(t)) %>% group_by(iter) %>% select(t, I1, I2) 
 
-# calculate the proportion of fadeouts, and the mean time to extinction of the parasite
-fadeoutT %>% group_by(beta2) %>% summarise(meant=mean(t), sdt=sd(t), N = length(t), prop = length(t)/nsims)
+# })[3]
+ptime
+stopCluster(cl)
 
+#### analyze output ####
+# stochastic prevalence mean as a function of beta*lambda0 and mu*rho
+read_csv(outfile, col_types = 'ddddd') %>% 
+  group_by(beta, mu) %>% summarise(meanprev=mean(prev),meant=mean(tinv, na.rm = T)) %>% 
+  ggplot(aes(beta*lambda0/(beta_orig*lambda0), mu*rho/(mu_orig*rho)))+
+  geom_contour_filled(aes(z=meanprev))+
+  theme_minimal(base_size = 14)+
+  labs(fill='Mean Prevalence', x=expression(paste(beta,lambda[0])),y=expression(paste(mu,rho)))
+
+# time to invasion as a function of beta*lambda0 and mu*rho 
+g3 <- read_csv(outfile, col_types = 'ddddd') %>% 
+  group_by(beta, mu) %>% summarise(meanprev=mean(prev),meant=mean(tinv, na.rm = T)) %>% 
+  ggplot(aes(beta*lambda0/(beta_orig*lambda0), mu*rho/(mu_orig*rho)))+
+  geom_contour_filled(aes(z=meant))+
+  theme_minimal(base_size = 12)+
+  labs(fill='Mean time', x=expression(paste(beta,lambda[0])),y=expression(paste(mu,rho)))
+
+# 95% CI of invasion times
+g4 <- read_csv(outfile, col_types = 'ddddd') %>% 
+  group_by(beta, mu) %>% summarise(mint=quantile(tinv, 0.025), maxt=quantile(tinv,0.975),tdif=maxt-mint) %>% 
+  ggplot(aes(beta*lambda0/(beta_orig*lambda0), mu*rho/(mu_orig*rho)))+
+  geom_contour_filled(aes(z=tdif))+
+  theme_minimal(base_size = 12)+
+  labs(fill='Time range', x=expression(paste(beta,lambda[0])),y=expression(paste(mu,rho)))
+
+# the only case where there is no invasion is if beta =0, otherwise, 100% of cases result in invasion.
+g2 <- read_csv(outfile, col_types = 'ddddd') %>% 
+  group_by(beta, mu) %>% summarise(meanprev=mean(prev), rangeprev=quantile(prev,0.975)-quantile(prev, 0.0225)) %>% 
+  ggplot(aes(beta*lambda0/(beta_orig*lambda0), mu*rho/(mu_orig*rho)))+
+  geom_contour_filled(aes(z=rangeprev), binwidth = 0.1)+
+  theme_minimal(base_size = 12)+
+  # theme(aspect.ratio = 1)+
+  labs(fill='Prev. range', x=expression(paste(beta,lambda[0])),y=expression(paste(mu,rho)))
+g2
+# range of prevalence
+read_csv(outfile, col_types = 'ddddd') %>% 
+  mutate(bdif=beta-beta_orig,mdif=mu-mu_orig) %>% 
+  filter(bdif==(abs(bdif)), mdif==min(abs(mdif))) %>% summarise(range(prev))
+
+# proportion of invasions
+read_csv(outfile, col_types = 'ddddd') %>% 
+  group_by(beta, mu) %>% mutate(inv=tinv>0) %>% 
+  mutate(bdif=beta-beta_orig,mdif=mu-mu_orig) %>% 
+  filter(bdif==min(abs(bdif)), mdif==min(abs(mdif))) %>% summarise(range(prev))
